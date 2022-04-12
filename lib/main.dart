164eaad5d40e16ba48e5f5d64550e49a8c8f4048
fawrama <bodygawad9@gmail.com>
2022-04-12 12:17:18 +0800

    adding colors to the interface

--- lib/main.dart
+++ lib/main.dart
@@ -19,32 +19,47 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 'enter the number and then press jump to:-)',
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              Container(
+                height: 20,
               ),
               TextField(
+                style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    hintText: 'enter a number between 0 or 5 '),
+                    hintText: 'enter a number between 0 or 5 ',
+                    hintStyle: TextStyle(color: Colors.white)),
                 onChanged: (value) => x = value,
+                cursorColor: Colors.white,
+              ),
+              Container(
+                height: 20,
               ),
               RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (x == null || x.isEmpty) x = '0';
-                      i = int.parse(x);
-                      if (i > 5) i = 5;
-                      if (i < 0) i = 0;
-                    });
-                  },
-                  child: Text('jump to ')),
+                onPressed: () {
+                  setState(() {
+                    if (x == null || x.isEmpty) x = '0';
+                    i = int.parse(x);
+                    if (i > 5) i = 5;
+                    if (i < 0) i = 0;
+                  });
+                },
+                child: Text('jump to '),
+              ),
+              Container(
+                height: 20,
+              ),
               Text(
                 friuts[i],
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, color: Colors.white),
               )
             ],
           ),
